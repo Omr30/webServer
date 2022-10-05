7bb4977c3126ce0e9884e28b533406b40d5d6e8a
mvrk <oscarmenacho3@gmail.com>
2022-10-05 09:37:16 -0500

    Servir un sitio web completo

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/hola-mundo', (req, res) => {
   res.send('Hola mundo en su respectiva ruta')
 })
 
+app.get('/elements', (req, res) => {
+res.sendFile( __dirname + '/public/elements.html' )
+})
+
+app.get('/generic', (req, res) => {
+res.sendFile( __dirname + '/public/generic.html' )
+})
+
 app.get('*', (req, res) => {
   res.sendFile( __dirname + '/public/404.html' )
 })
