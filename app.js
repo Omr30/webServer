74677a56c90439cc74df0a8dcb15d199c34a8f3d
mvrk <oscarmenacho3@gmail.com>
2022-10-05 11:24:34 -0500

    Usando parciales con HBS

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
 const port = 8080;
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
 
 // Servir contenido estatico
 app.use( express.static('public') );
@@ -15,15 +18,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/elements', (req, res) => {
-    res.sendFile( __dirname + '/public/elements.html' )
+    res.render('elements', {
+        nombre: 'Mvrk',
+        titulo: 'Curso de Node'
+    })
 })
 
 app.get('/generic', (req, res) => {
-    res.sendFile( __dirname + '/public/generic.html' )
+    res.render('generic', {
+        nombre: 'Mvrk',
+        titulo: 'Curso de Node'
+    })
 })
 
 app.get('*', (req, res) => {
-    res.sendFile( __dirname + '/public/404.html' )
+    res.sendFile( __dirname + '/public/back/404.html' )
 })
 
 app.listen(port, () => {
