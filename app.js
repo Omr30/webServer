0a597b692ed08de46e5fb3c5675bb4dddb1ce2ae
mvrk <oscarmenacho3@gmail.com>
2022-10-05 09:49:04 -0500

    Handlebars

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+app.set('view engine', 'hbs');
+
 // Servir contenido estatico
 app.use( express.static('public') );
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hola mundo en su respectiva ruta')
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
 app.get('/elements', (req, res) => {
-res.sendFile( __dirname + '/public/elements.html' )
+    res.sendFile( __dirname + '/public/elements.html' )
 })
 
 app.get('/generic', (req, res) => {
-res.sendFile( __dirname + '/public/generic.html' )
+    res.sendFile( __dirname + '/public/generic.html' )
 })
 
 app.get('*', (req, res) => {
-  res.sendFile( __dirname + '/public/404.html' )
+    res.sendFile( __dirname + '/public/404.html' )
 })
 
 app.listen(port, () => {
